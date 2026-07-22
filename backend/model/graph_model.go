20a94c0050e9078a37d9cbc2aadb7460e1a5196b
Abhay_Yarasi <129658168+ImmortalAbdu413@users.noreply.github.com>
2026-07-22 17:35:33 +0930

    Innit model

--- backend/model/graph_model.go
+++ backend/model/graph_model.go
@@ -1,1 +1,17 @@
 package model
+
+type Graph struct{
+	Name string
+}
+
+type GraphManager interface{
+	CreateGraph(Name string)
+}
+
+type JoojGraphManager struct{
+	InMemoryStore map[string]Graph 
+}
+
+func (j JoojGraphManager) CreateGraph(Name string){
+	//  Todo
+}
--- backend/test/graph_test.go
+++ backend/test/graph_test.go
@@ -1,3 +1,2 @@
 package test
 
-// Todo
